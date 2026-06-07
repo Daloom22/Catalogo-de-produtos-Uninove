c2e9d24ae6af36c4708ffc89bac9e2452b0d5aa3
Davi Lopes Omena <davilomena@gmail.com>
2026-06-07 02:58:42 +0000

    Criação do gitignore

--- obj/Debug/net10.0/Catalogo-de-produtos-Uninove.AssemblyInfo.cs
+++ obj/Debug/net10.0/Catalogo-de-produtos-Uninove.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Catalogo-de-produtos-Uninove")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33a0dc190d07a5d9d55f30b44254d18acdd628d7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1c4e327fc31eef9c89f266d026f5be4f7d23d4d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Catalogo-de-produtos-Uninove")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Catalogo-de-produtos-Uninove")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
